2a198ab1af1766830e2f7f47428aec0de542fa34
vardaasen <git@vardaasen.no>
2025-09-20 17:55:04 +0200

    style

--- draw_plot.py
+++ draw_plot.py
@@ -14,40 +14,61 @@ def draw_plot(b):
     GRAPH_DATA_STEP = 0.1
     GRAPH_STD = GRAPH_DATA_STEP
     EMPIRICAL_Y_LIM = {min: (-4 * GRAPH_STD) 
-                       + min(np.sin(np.arange(GRAPH_DATA[min],
-                                              GRAPH_DATA[max],
-                                              GRAPH_DATA_STEP))),
+                       + min(np.sin(np.arange(
+                           GRAPH_DATA[min],
+                           GRAPH_DATA[max],
+                           GRAPH_DATA_STEP,
+                           ))),
                        max: (4 * GRAPH_STD) 
-                       + max(np.sin(np.arange(GRAPH_DATA[min],
-                                              GRAPH_DATA[max],
-                                              GRAPH_DATA_STEP)))
+                       + max(np.sin(np.arange(
+                           GRAPH_DATA[min],
+                           GRAPH_DATA[max],
+                           GRAPH_DATA_STEP,
+                           )))
                       }
     data = {
-        'X': np.arange(GRAPH_DATA[min], 
-                       GRAPH_DATA[max], 
-                       GRAPH_DATA_STEP),
-        'Y': np.sin(np.arange(GRAPH_DATA[min], 
-                              GRAPH_DATA[max], 
-                              GRAPH_DATA_STEP)) 
-        + np.random.normal(GRAPH_MEAN, 
-                           GRAPH_STD, 
-                           len(np.arange(GRAPH_DATA[min], 
-                                         GRAPH_DATA[max], 
-                                         GRAPH_DATA_STEP)))
+        'X': np.arange(
+            GRAPH_DATA[min], 
+            GRAPH_DATA[max], 
+            GRAPH_DATA_STEP,
+            ),
+        'Y': np.sin(np.arange(
+            GRAPH_DATA[min], 
+            GRAPH_DATA[max], 
+            GRAPH_DATA_STEP,
+            )) 
+        + np.random.normal(
+            GRAPH_MEAN, 
+            GRAPH_STD, 
+            len(np.arange(
+                GRAPH_DATA[min], 
+                GRAPH_DATA[max], 
+                GRAPH_DATA_STEP,
+                )))
         }
 
     df = pd.DataFrame(data)
 
-    slope, intercept, r_value, p_value, std_err = stats.linregress(df['X'], df['Y'])
+    (
+        slope, 
+        intercept, 
+        r_value, 
+        p_value, 
+        std_err,
+    ) = stats.linregress(df['X'], df['Y'])
     regression_line = slope * df['X'] + intercept
     formula_text = f'y = {slope:.2f}x {intercept:+.2f}'
 
     ax.clear()
-    ax.plot(df['X'], df['Y'], label='Original Data')
-    ax.plot(df['X'], regression_line, color='red', linestyle='--', label='Linear Regression')
-    ax.set_title('Sine Wave with Noise and Regression')
-    ax.set_xlabel('X-axis')
-    ax.set_ylabel('Y-axis')
+    ax.plot(df['X'], df['Y'], label=f'Original Data')
+    ax.plot(df['X'],
+            regression_line,
+            color='red',
+            linestyle='--',
+            label=f'Linear Regression')
+    ax.set_title(f'Sine Wave with Noise and Regression')
+    ax.set_xlabel(f'X-axis')
+    ax.set_ylabel(f'Y-axis')
     ax.legend()
     ax.set_xlim(GRAPH_DATA[min], GRAPH_DATA[max])
     ax.set_ylim(EMPIRICAL_Y_LIM[min], EMPIRICAL_Y_LIM[max])
@@ -65,10 +86,12 @@ def draw_plot(b):
 
 %matplotlib widget
 
-fig, ax = plt.subplots(figsize=(5,4))
-ax.set_title("Here is the button you click")
+fig, ax = plt.subplots(figsize = (5,4))
+ax.set_title(f'Here is the button you click')
 
-plot_button = widgets.Button(description="Generate and Display Graph")
+plot_button = widgets.Button(description = (
+    f'Generate and Display Graph'
+    ))
 plot_button.on_click(draw_plot)
 display(plot_button)
 
